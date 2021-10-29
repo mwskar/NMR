5a35e94a10b56fb7834fac3f8b4d1c49e72589b7
Michael W. Skarbek <skarbek_mw@cobra.cs.mercer.edu>
2021-10-29 16:45:26 -0400

    Fixed romberg integration

--- rtest.f95
+++ rtest.f95
@@ -1,36 +1,66 @@
 program rom
 
-double precision :: low, up, calcSpline
+double precision :: low, up
 double precision :: intsum, ans
-double precision :: h, arr(0:10,0:10)
+double precision :: h, arr(1:2,1:10)
 integer :: i
+logical :: run
+
 
 low = 2
 up = 4
-tolerance = 1E-2
+tolerance = 1E-5
 ans = -1.0D0
 
-i = 1
+run = .true.
 
-h = low - up
-arr(0,0) = (h/2) * (calcSpline(low) + calcSpline(up))
+print *, "Tol: ", tolerance
 
-do while ( (arr(1,i-1) - arr(2,i) ) >= tolerance )
+h = up - low
+
+print *, "Up: ", up
+
+arr(1,1) =(h/2) * (calcSpline(low) + calcSpline(up))
+
+print *, arr(1,1)
+
+
+!arr(2,:) = 0
+
+!print *, arr(1,:)
+!print *, arr(2,:)
+
+
+!print *, "Test case: ", arr(1,i-1) - arr(2,i)
+
+
+i = 2
+do while (i <= 10 .and. run )
+    print *, "Tol check : ", arr(1,i-1) - arr(2,i)
     intsum = 0.0D0
     do k = 1, 2**(i-2)
         intsum = intsum + calcSpline(low + (k-0.5D0)*h)
+        !print *, "Sum: ", intsum
     end do
     
-    arr(2,0) = (1/2) * ( arr(i-1,i-1) + h * intsum)
-
-    do j = 1, i
+    arr(2,1) = (0.5D0) * ( arr(1,1) + h * intsum)
+    !print *, "new root: ", arr(2,1)
+    do j = 2, i
         arr(2,j) = arr(2,j-1) + ( (arr(2,j-1) - arr(1,j-1)) / (4**(j-1) -1) )
     end do
-    i = i +1
 
-    do l=0,10
+    print *, arr(2,1:i)
+
+
+    if (abs(arr(1,i-1) - arr(2,i) ) < tolerance) run = .false.
+
+    do l=1,10
         arr(1,l) = arr(2,l)
     end do
+
+
+    h = h / 2
+    i = i + 1
 
 end do
 
